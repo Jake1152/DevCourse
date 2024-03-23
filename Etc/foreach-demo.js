66c41092698a193b4f48a7b5ac6b2105c5f42132
Jake1152 <rancho974@gmail.com>
2024-03-23 21:09:26 +0900

    [ADD] foreach with map

--- Etc/foreach-demo.js
+++ Etc/foreach-demo.js
@@ -35,3 +35,22 @@ arr.forEach((value, index, all, other) => {
 // arr.forEach((element) = {
 //     console.log(`element : ${element}`)
 // });
+
+let map = new Map();
+map.set(7, "seven");
+map.set(9, "nine");
+map.set(8, "eight");
+
+map.forEach(function (value) {
+  console.log(`value : ${value}`);
+});
+
+map.forEach(function (value, key) {
+  console.log(`value : ${value}, key: ${key}`);
+});
+
+// Map인 경우에 마지막 인자는 map 객체가 통째로 들어간다.
+// value, key, map object
+map.forEach(function (value, key, mapObjects) {
+  console.log(`value : ${value}, key: ${key}, mapObjects: ${mapObjects}`);
+});
