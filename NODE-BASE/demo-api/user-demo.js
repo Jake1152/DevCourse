7a772fc6b6299dcea41b71f94c78b176b153732b
Jake1152 <rancho974@gmail.com>
2024-03-26 02:02:14 +0900

    [FIX, ADD] 회원가입 API 수정, 회원 탈퇴 API 추가
    - if (req.body == {}) 비교식은 참조비교이므로 항상 false를 반환하는
      문제가 있었다.
      Objects.key(req.body).length === 0을 이용하여 처리
    - 회원탈퇴를 위한 HTTP DELETE Method 를 사용하는 API 추가

--- NODE-BASE/demo-api/user-demo.js
+++ NODE-BASE/demo-api/user-demo.js
@@ -30,7 +30,6 @@ app.post("/login", (req, res) => {
 // join
 app.post("/join", (req, res) => {
   console.log(`req.body : `, req.body);
-  console.log(`req.body.size : `, req.body.size);
 
   let statusCode;
   let msg;
@@ -38,25 +37,21 @@ app.post("/join", (req, res) => {
   // req.body는 항상 값이 있기에 req.body === undefined 같은 코드가 동작하지 않는다.
   // if (req.body === undefined) {
   // == {}이면 === {} 일때와 무엇이 다른가? 둘다 false가 나온다. 의도대로 동작하지 않는 코드이다.
-  if (req.body == {}) {
+  // if (req.body == {}) {
+  // if (!req.body) {
+  if (Object.keys(req.body).length === 0) {
+    statusCode = 400;
+    msg = `회원정보를 입력해주세요.`;
     // if (req.body.size > 0) {
+  } else {
     db.set(myId++, req.body);
     statusCode = 201;
     msg = `${db.get(myId - 1).name}님 환영합니다.`;
-  } else {
-    statusCode = 400;
-    msg = `회원정보를 입력해주세요.`;
   }
 
   res.status(statusCode).json({
     message: msg,
   });
-  // if (req.body == {}) {
-  //   // res.status(StatusCodes.BAD_REQUEST).json({
-  //   res.status(400).json({
-  //     message: "입력 값을 다시 확인해주세요.",
-  //   });
-  // }
 });
 
 // 특정유저 찾기
@@ -74,24 +69,39 @@ app.get("/users/:id", (req, res) => {
     msg = `찾으시는 유저를 못 찾았습니다.`;
   }
 
-  res.status(200).send({ message: msg });
+  return res.status(200).send({ message: msg });
 });
 
 // 전체유저 목록 반환
-app.get("/users", (req, res) => {
-  console.log(`db : `, db);
-  let users = {};
-  if (db.size >= 1) {
-    users = db;
-    res.status(200).json(db);
-  }
-  // if (db.get(id)) {
-  //   // if (db.get(id)) {
-  //   // const name = db.find;
-  //   msg = `${db.get(id).name} 유저를 찾았습니다.`;
-  // } else {
-  //   msg = `${id} 유저를 못 찾았습니다.`;
-  // }
+// app.get("/users", (req, res) => {
+//   console.log(`db : `, db);
+//   let users = {};
+//   if (db.size >= 1) {
+//     users = db;
+//     return res.status(200).json(db);
+//   }
+//   // if (db.get(id)) {
+//   //   // if (db.get(id)) {
+//   //   // const name = db.find;
+//   //   msg = `${db.get(id).name} 유저를 찾았습니다.`;
+//   // } else {
+//   //   msg = `${id} 유저를 못 찾았습니다.`;
+//   // }
 
-  res.status(200).json(users);
+//   return res.status(200).json(users);
+// });
+
+app.delete("/users/:id", (req, res) => {
+  let msg = "";
+
+  let { id } = req.params;
+  id = parseInt(id);
+
+  if (db.get(id)) {
+    msg = `${db.get(id).name} 님 탈퇴 되셨습니다.`;
+    db.delete(id);
+  } else {
+    msg = `회원가입 되지 않은 정보입니다.`;
+  }
+  return res.status(200).json({ message: msg });
 });
