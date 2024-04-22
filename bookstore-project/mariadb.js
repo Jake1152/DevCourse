c9501ab46e1dbb30901d2aa36a2a3cefaff6b3ad
Jake1152 <rancho974@gmail.com>
2024-04-22 10:23:30 +0900

    [FIX] use dotenv when use DB

--- bookstore-project/mariadb.js
+++ bookstore-project/mariadb.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root1234",
-  database: "bookstore",
-  dateStrings: true,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dateStrings: true, // 시간대 맞추기
 });
 
 module.exports = connection;
