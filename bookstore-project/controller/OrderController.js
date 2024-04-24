c03b67325dde54bbe665945bfaeae06b75114744
Jake1152 <rancho974@gmail.com>
2024-04-25 02:35:18 +0900

    [ADD] 주문 조회, 주문 내역 조회

--- bookstore-project/controller/OrderController.js
+++ bookstore-project/controller/OrderController.js
@@ -13,10 +13,9 @@ const order = async (req, res) => {
     database: process.env.DB_NAME,
     dateStrings: true,
   });
+
   const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
     req.body;
-
-  let result = await deleteCartItems(conn);
 
   let sql =
     "INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)";
@@ -32,38 +31,87 @@ const order = async (req, res) => {
 
   [results] = await conn.query(sql, values);
   let order_id = results.insertId;
+
   console.log("order_id : ", order_id);
 
-  // orderedBook 테이블 삽입
+  // SELCT book_id, quantity FROM cartItmes WHERE IN [1,2,3]
+  // items를 가지고 장바구니에서 book_id, quantity 조회
+  sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+  console.log("sql : ", sql);
 
+  let [orderItems, fields] = await conn.query(sql, [items]);
+  console.log("order_id : ", order_id);
+  // let orderItems = await conn.query(sql, [items]);
+
+  console.log("orderItems : ", orderItems);
+  console.log("fields : ", fields);
+  // orderedBook 테이블 삽입
   sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
 
   values = [];
-  items.forEach((item) => {
+  orderItems.forEach((item) => {
     values.push([order_id, item.book_id, item.quantity]);
   });
+  console.log("values : ", values);
 
   results = await conn.query(sql, [values]);
   console.log("results : ", results);
 
-  return res.status(StatusCodes.OK).json(results[0]);
+  const result = await deleteCartItems(conn, items);
+
+  return res.status(StatusCodes.OK).json(result);
 };
 
-const getOrders = (req, res) => {
-  res.status(StatusCodes.OK).json({ message: "getOrders" });
-};
+/**
+ * execute vs query 차이점
+ */
+const deleteCartItems = async (conn, items) => {
+  const sql = `DELETE FROM cartItems WHERE id IN (?)`;
 
-const getOrderDetail = (req, res) => {
-  res.status(StatusCodes.OK).json({ message: "getOrderDetail" });
-};
-
-const deleteCartItems = async (conn) => {
-  const sql = `DELETE FROM cartItems WEHRE id IN (?)`;
-  let values = [1, 2, 3];
-
-  let result = await conn.execute(sql, values);
+  // const result = await conn.execute(sql, [items]);
+  const result = await conn.query(sql, [items]);
   return result;
   // return res.status(StatusCodes.OK).json(result);
+};
+
+const getOrders = async (req, res) => {
+  const conn = await mariadb.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dateStrings: true,
+  });
+
+  const sql = `SELECT orders.id, created_at, address, receiver, contact,
+                      book_title, total_quantity, total_price
+                FROM orders 
+                LEFT JOIN delivery
+                ON orders.delivery_id = delivery.id`;
+  const [rows, fields] = await conn.query(sql);
+
+  res.status(StatusCodes.OK).json(rows);
+  // res.status(StatusCodes.OK).json({ message: "getOrders" });
+};
+
+const getOrderDetail = async (req, res) => {
+  const { id } = req.params;
+  const conn = await mariadb.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dateStrings: true,
+  });
+
+  const sql = `SELECT book_id, title, author, price, quantity
+                FROM orderedBook
+                LEFT JOIN books
+                ON orderedBook.book_id = books.id`;
+  const [rows, fields] = await conn.query(sql);
+
+  res.status(StatusCodes.OK).json(rows);
+  // res.status(StatusCodes.OK).json({ message: "getOrders" });
 };
 
 // const addToCart = (req, res) => {
