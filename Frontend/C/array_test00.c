ba02b34afa307dda92dccd960930bd17b5dc4d68
Jake1152 <rancho974@gmail.com>
2024-05-01 12:56:27 +0900

    [FIX] sizeof test, custom sizeof
    - #define my_sizeof(x) ((char *)(&x + 1) - (char *)&x)

--- Frontend/C/array_test00.c
+++ Frontend/C/array_test00.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define my_sizeof(x) ((char *)(&x + 1) - (char *)&x)
+
+
 static int read_pages = 0;
 
 void add_read_pages(const int pages)
@@ -65,8 +68,23 @@ int *reverse(const int *arr_origin, size_t arr_size)
 */
 int main()
 {
+    // #define my_sizeof(x) ((char *)(&x + 1) - (char *)&x)
+    /*
+    * # 배열의 크기 계산이 가능한 이유
+    * - size_t arr_size = my_sizeof(arr_origin) / my_sizeof(arr_origin[0]);
+    *   - 가능한 이유는 sizeof가 전체를 구조체로 보기 때문에 그러하다.
+    *   - 구조체의 단위
+    *   - #define my_sizeof(x) ((char *)(&x + 1) - (char *)&x)
+    *   - (char *)(&x + 1)의 의미 
+    *     - Q. 왜 주소값을 구하고서 1을 더한 뒤에 (char *)로 캐스팅할까?
+    *       - 이유는 &x 를 해야 해당 구조체 단위로 계산되고 그 계산한 값에 1을 더하니까 그 구조체의 바이트 크기만큼 증가하기 때문이다.
+    *       - int 였다면 4byte이고 배열 이름의 경우 배열의 전체크기 단위로 계산된다 arr_origin = {1,2,3,4,5}
+    *         - arr_origin은 4bytes 데이터 타입의 값이 5개라서 20bytes로 계산된다.
+    */
     int arr_origin[] = {1,2,3,4,5};
-    size_t arr_size = sizeof(arr_origin) / sizeof(arr_origin[0]);
+    size_t arr_size = my_sizeof(arr_origin) / my_sizeof(arr_origin[0]);
+
+    // arr_origin == arr_origin[0]
 
     int *arr_clone = reverse(arr_origin, arr_size);
     // int *arr_clone = reverse(arr_origin);
