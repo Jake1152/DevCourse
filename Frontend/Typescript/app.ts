55c0e861e68566463cc9ffdcbc3a7fc446ca6fca
Jake1152 <rancho974@gmail.com>
2024-05-13 10:48:37 +0900

    [ADD] interface, setting

---
+++ Frontend/Typescript/app.ts
@@ -0,0 +1,17 @@
+// 변수의 데이터 타입 명시
+
+let age: number = 20;
+let gender: string = "male";
+let course: string = "Typescript";
+let completed: boolean = false;
+
+function getInfo(id: number): {
+  stdId: number;
+  stdName: string;
+  age: number;
+  gender: string;
+  course: string;
+  completed: boolean;
+} {
+  return null;
+}
