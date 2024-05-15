a60a6eed9d5529131b7a15a1152b8eecd8d98e64
Jake1152 <rancho974@gmail.com>
2024-05-16 01:05:22 +0900

    [ADD] React project

---
+++ Frontend/React/todolist/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1 className="test">Hello, React!!</h1>
+      <p> Welcome</p>
+    </div>
+  );
+}
+
+export default App;
