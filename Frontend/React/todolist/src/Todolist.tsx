840c5e68ea9d0a97d0032f042041c1fd5fe51091
Jake1152 <rancho974@gmail.com>
2024-05-17 00:43:16 +0900

    [ADD] Destructuring with type alias

--- Frontend/React/todolist/src/Todolist.tsx
+++ Frontend/React/todolist/src/Todolist.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+type Todo = {
+  id: number;
+  text: string;
+  isChecked: boolean;
+};
 
 const TodoList: React.FC = () => {
   const title: string = "TODO";
 
-  const [todos] = useState(["공부하기", "잠자기", "미팅하기"]);
+  // Destructuring
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, text: "공부하기", isChecked: false },
+    { id: 2, text: "잠자기", isChecked: false },
+    { id: 3, text: "미팅하기", isChecked: false },
+  ]);
   return (
     <div>
       <h1> {title} </h1>
       <p></p>
       <div className="container">
         <ul>
-          <li>{todos[0]}</li>
-          <li>{todos[1]}</li>
-          <li>{todos[2]}</li>
+          <li>{todos[0].text}</li>
+          <li>{todos[1].text}</li>
+          <li>{todos[2].text}</li>
         </ul>
       </div>
     </div>
