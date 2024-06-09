8a46d7e4c577ee5ebbf63c3d8e8a4c187db875c9
Jake1152 <rancho974@gmail.com>
2024-06-09 23:09:07 +0900

    Feat: Layout 세팅
    - Layout 2가지 방식
      - 태그로 감싸기
      - 인자로 태그를 넘겨주기

---
+++ Frontend/React/book-store-v/src/components/common/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <>
+      <hr />
+      <div>copyright(c), 20204, book store.</div>
+    </>
+  );
+}
+
+export default Footer;
--- Frontend/React/book-store-v/src/pages/Home.tsx
+++ Frontend/React/book-store-v/src/pages/Home.tsx
@@ -1,12 +1,9 @@
-import Header from "../components/Header";
 import { formatNumber } from "../utils/format";
-
 const COUNT = 4242;
 
 function Home() {
   return (
     <>
-      <Header />
       <div> home body </div>
       <div> COUNT: {formatNumber(COUNT)} </div>
     </>
