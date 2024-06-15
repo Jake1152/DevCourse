65edef7b5fddb5d043230952e5e6d72f641dba4b
Jake1152 <rancho974@gmail.com>
2024-06-15 22:46:29 +0900

    Feat: Global style 적용
    - sanitize.css
      - global 적용, /src/styled/global.ts, /src/main.tsx
    - styled-component

--- Frontend/React/book-store-v/src/components/common/Header.tsx
+++ Frontend/React/book-store-v/src/components/common/Header.tsx
@@ -1,9 +1,23 @@
+import { styled } from "styled-components";
+
 function Header() {
   return (
-    <header>
+    <HeaderStyle>
       <h1>book store</h1>
-    </header>
+    </HeaderStyle>
   );
 }
+
+/**
+ * 일반적으로 styled-component는 분리해서 구현한다.
+ * 여기서는 확인용도라서 같은 파일에 위치한다.
+ */
+const HeaderStyle = styled.header`
+  background-color: #333;
+
+  h1 {
+    color: white;
+  }
+`;
 
 export default Header;
